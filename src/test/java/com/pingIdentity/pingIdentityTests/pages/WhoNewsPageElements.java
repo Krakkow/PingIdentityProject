41122a2df21376e92b493c84c75661c6d94c0cf4
Or Kowalsky <okowalsky@gmail.com>
2025-07-10 15:00:13 +0300

    News Page Elemetns

--- src/test/java/com/pingIdentity/pingIdentityTests/pages/WhoNewsPageElements.java
+++ src/test/java/com/pingIdentity/pingIdentityTests/pages/WhoNewsPageElements.java
@@ -1,5 +1,19 @@
 package com.pingIdentity.pingIdentityTests.pages;
 
+import org.openqa.selenium.By;
+
 public class WhoNewsPageElements {
+
+    public static final String EXPECTED_TAB_TITLE = "News";
+    public static final String EXPECTED_URL = "https://www.who.int/news";
+
+    public static final By PAGE_TITLE_HEADING = By.xpath("//h1[contains(text(), 'News')]");
+    public static final By PAGE_SUBTITLE_HEADING = By.xpath("//p[contains(text(), 'Browse selected WHO news below' )]");
+    public static final By NEWS_TYPE_DROPDOWN = By
+            .xpath("//input[@placeholder='News type']/ancestor::span[contains(@class,'k-dropdown-wrap')]");
+    public static final By NEWS_ARTICLE_BLOCKS = By
+            .xpath("//div[contains(@class, 'list-view--item vertical-list-item')]");
+    public static final By ARTICLE_TITLE = By.xpath(".//a[contains(@class, 'link-container')]");
+    public static final By ARTICLE_URL = By.xpath(".//a[contains(@class, 'link-container')]");
 
 }
